8da26632797ec9b7ef84b403db0d1293e5c6b849
jtejido <jtbibliomania@gmail.com>
2021-01-29 06:34:23 +0800

    add keywordattribute initially required by English analyzer

---
+++ core/analysis/tokenattributes/keyword.go
@@ -0,0 +1,42 @@
+package tokenattributes
+
+import (
+	"github.com/jtejido/golucene/core/util"
+)
+
+// This attribute can be used to mark a token as a keyword. Keyword aware TokenStreams can decide
+// to modify a token based on the return value of isKeyword() if the token is modified.
+// Stemming filters for instance can use this attribute to conditionally skip a term if isKeyword() returns true.
+type KeywordAttribute interface {
+	// Returns true if the current token is a keyword, otherwise false.
+	IsKeyword() bool
+
+	// Marks the current token as keyword if set to true.
+	SetKeyword(isKeyword bool)
+}
+
+/* Default implementation of CharTermAttribute. */
+type KeywordAttributeImpl struct {
+	keyword bool
+}
+
+func NewKeywordAttributeImpl() *KeywordAttributeImpl {
+	return new(KeywordAttributeImpl)
+}
+
+func (a *KeywordAttributeImpl) Clear() {
+	a.keyword = false
+}
+
+func (a *KeywordAttributeImpl) CopyTo(target util.AttributeImpl) {
+	attr := target.(KeywordAttribute)
+	attr.SetKeyword(a.keyword)
+}
+
+func (a *KeywordAttributeImpl) IsKeyword() bool {
+	return a.keyword
+}
+
+func (a *KeywordAttributeImpl) SetKeyword(isKeyword bool) {
+	a.keyword = isKeyword
+}
